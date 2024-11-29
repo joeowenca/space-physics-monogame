a05ca84d89b283272957515f0dae82a54bde07d5
Joe Owen <joe.a.owen@gmail.com>
2024-11-28 23:10:23 -0800

    Create LoopingBackground class

---
+++ SpacePhysics/SpacePhysics/Sprites/LoopingBackground.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpacePhysics.Sprites;
+
+public class LoopingBackground : CustomGameComponent
+{
+  private readonly Func<Color> color;
+  private float parallaxFactor;
+
+  public LoopingBackground(Texture2D texture, Func<Color> color, int layerIndex) : base(Alignment.TopLeft, layerIndex)
+  {
+    this.texture = texture;
+    this.color = color;
+    parallaxFactor = layerIndex * 0.071375f;
+  }
+
+  public override void Draw(SpriteBatch spriteBatch)
+  {
+    spriteBatch.Draw(texture, new Rectangle(0, 0, 0, 0), color());
+  }
+}
--- SpacePhysics/SpacePhysics/obj/Debug/net8.0/SpacePhysics.AssemblyInfo.cs
+++ SpacePhysics/SpacePhysics/obj/Debug/net8.0/SpacePhysics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpacePhysics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde138ded6edf698c9e473c0cf872022f320fe01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559fbb1d8bd023610004d499e8bba85926075bbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpacePhysics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpacePhysics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
