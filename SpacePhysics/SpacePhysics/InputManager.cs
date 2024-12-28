68814283f48b99c793f7d4310cc1d997b4290859
Joe Owen <joe.a.owen@gmail.com>
2024-12-28 13:26:56 -0800

    Add controller methods to InputManager

--- SpacePhysics/SpacePhysics/InputManager.cs
+++ SpacePhysics/SpacePhysics/InputManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace SpacePhysics;
@@ -7,20 +9,34 @@ public class InputManager
 {
   private KeyboardState previousKeyboardState;
   private KeyboardState currentKeyboardstate;
+
+  private GamePadState previousGamePadState;
+  private GamePadState currentGamePadState;
+
   public bool allowInput;
+
+  public readonly bool gamePadConnected;
 
   public InputManager(bool allowInput = true)
   {
     this.allowInput = allowInput;
 
+    gamePadConnected = false;
+
     previousKeyboardState = Keyboard.GetState();
     currentKeyboardstate = Keyboard.GetState();
+
+    previousGamePadState = GamePad.GetState(PlayerIndex.One);
+    currentGamePadState = GamePad.GetState(PlayerIndex.One);
   }
 
   public void Update()
   {
     previousKeyboardState = currentKeyboardstate;
     currentKeyboardstate = Keyboard.GetState();
+
+    previousGamePadState = currentGamePadState;
+    currentGamePadState = GamePad.GetState(PlayerIndex.One);
   }
 
   public bool OnFirstFramePress(Keys key)
@@ -35,5 +51,82 @@ public class InputManager
     if (!allowInput) return false;
 
     return currentKeyboardstate.IsKeyDown(key);
+  }
+
+  public void ControllerRumble(float intensity)
+  {
+    GamePad.SetVibration(PlayerIndex.One, intensity, intensity);
+  }
+
+  public (Vector2 Left, Vector2 Right) AnalogStick()
+  {
+    return
+    (
+      currentGamePadState.ThumbSticks.Left,
+      currentGamePadState.ThumbSticks.Right
+    );
+  }
+
+  public (float Left, float Right) Trigger()
+  {
+    return
+    (
+      currentGamePadState.Triggers.Left,
+      currentGamePadState.Triggers.Right
+    );
+  }
+
+  public (ButtonState left, ButtonState right) Bumper()
+  {
+    return
+      (
+        currentGamePadState.Buttons.LeftShoulder,
+        currentGamePadState.Buttons.LeftShoulder
+      );
+  }
+
+  public
+  (
+    ButtonState A,
+    ButtonState B,
+    ButtonState X,
+    ButtonState Y
+  )
+    FaceButton()
+  {
+    return
+    (
+      currentGamePadState.Buttons.A,
+      currentGamePadState.Buttons.B,
+      currentGamePadState.Buttons.X,
+      currentGamePadState.Buttons.Y
+    );
+  }
+
+  public
+  (
+    ButtonState Left,
+    ButtonState Right,
+    ButtonState Up,
+    ButtonState Down
+  )
+    DPad()
+  {
+    return
+    (
+      currentGamePadState.DPad.Left,
+      currentGamePadState.DPad.Right,
+      currentGamePadState.DPad.Up,
+      currentGamePadState.DPad.Down
+    );
+  }
+
+  public (ButtonState start, ButtonState select) Menu()
+  {
+    return
+      (
+        currentGamePadState.Buttons.Start,
+        currentGamePadState.Buttons.Back
+      );
   }
 }
