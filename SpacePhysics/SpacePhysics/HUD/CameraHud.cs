74def0199fe28abbdae558af23a28e4eed5050b1
Joe Owen <joe.a.owen@gmail.com>
2024-12-29 15:41:52 -0800

    Add CameraAngleText UI

--- SpacePhysics/SpacePhysics/HUD/CameraHud.cs
+++ SpacePhysics/SpacePhysics/HUD/CameraHud.cs
@@ -9,9 +9,42 @@ public class CameraHud : CustomGameComponent
 {
     private Vector2 offset;
 
-    public CameraHud(Func<float> opacity) : base(false, Alignment.TopCenter, 11)
+    private string cameraAngleText;
+
+    public CameraHud(Func<float> opacity) : base(true, Alignment.TopCenter, 11)
     {
         offset = new Vector2(0, 350f);
+
+        cameraAngleText = "Horizon";
+
+        components.Add(new HudText(
+            "Fonts/text-font",
+            () => "Camera: ",
+            Alignment.TopCenter,
+            TextAlign.Left,
+            () => new Vector2(0f, 0f) + offset,
+            () => defaultColor * opacity(),
+            hudTextScale,
+            11
+        ));
+
+        components.Add(new HudText(
+            "Fonts/text-font",
+            () => cameraAngleText,
+            Alignment.TopCenter,
+            TextAlign.Left,
+            () => new Vector2(components[0].width, 0f) + offset,
+            () => highlightColor * opacity(),
+            hudTextScale,
+            11
+        ));
+    }
+
+    public override void Update()
+    {
+        cameraAngleText = Camera.Camera.changeCamera ? "Ship" : "Horizon";
+
+        base.Update();
     }
 
     public override void Draw(SpriteBatch spriteBatch)
