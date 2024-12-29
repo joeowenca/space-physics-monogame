f44c38c80689e9ebc44803a91a08b09a013a0890
Joe Owen <joe.a.owen@gmail.com>
2024-12-29 14:50:09 -0800

    Remove CameraHud logic from SpaceScene

--- SpacePhysics/SpacePhysics/HUD/CameraHud.cs
+++ SpacePhysics/SpacePhysics/HUD/CameraHud.cs
@@ -12,43 +12,6 @@ public class CameraHud : CustomGameComponent
     public CameraHud(Func<float> opacity) : base(false, Alignment.TopCenter, 11)
     {
         offset = new Vector2(0, 350f);
-
-        HudSprite meter = new(
-            "HUD/meter-left",
-            Alignment.TopCenter,
-            Alignment.Center,
-            () => offset,
-            () => -(float)Math.PI * 0.5f,
-            () => defaultColor * opacity(),
-            hudScale,
-            11
-        );
-
-        HudSprite indicator = new(
-            "HUD/meter-indicator-left",
-            Alignment.TopCenter,
-            Alignment.Center,
-            () => new Vector2((zoomPercent * 6.28f) - 628f, 0) + offset,
-            () => -(float)Math.PI * 0.5f,
-            () => highlightColor * opacity(),
-            hudScale,
-            11
-        );
-
-        HudText percent = new(
-            "Fonts/text-font",
-            () => zoomPercent.ToString("0") + "%",
-            Alignment.TopCenter,
-            TextAlign.Center,
-            () => new Vector2(0, 120f) + offset,
-            () => highlightColor * opacity(),
-            hudTextScale,
-            11
-        );
-
-        components.Add(meter);
-        components.Add(indicator);
-        components.Add(percent);
     }
 
     public override void Draw(SpriteBatch spriteBatch)
