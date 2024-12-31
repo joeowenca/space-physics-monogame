a8b115367c0d37edf32351b11bb7ea9a60d44353
Joe Owen <joe.a.owen@gmail.com>
2024-12-30 19:45:57 -0800

    Replace MenuItems with ControlItems

--- SpacePhysics/SpacePhysics/Menu/ControlsMenu.cs
+++ SpacePhysics/SpacePhysics/Menu/ControlsMenu.cs
@@ -29,7 +29,7 @@ public class ControlsMenu : CustomGameComponent
       layerIndex
     )
   {
-    entireOffsetOverride = new Vector2(-450f, -600f);
+    entireOffsetOverride = new Vector2(-600f, -850f);
     offset = new Vector2(menuOffsetXRight, 0f);
     baseOffset = offset;
     controlItemDistance = 1750f;
@@ -46,102 +46,142 @@ public class ControlsMenu : CustomGameComponent
     ));
 
     components.Add(new ControlItem(
-        "Adjust Pitch",
-        () => "Left Stick",
-        () => activeMenu == 1,
-        alignment,
-        () => new Vector2(0f, 0f) + menuOffsetOverride + entireOffsetOverride,
-        controlItemDistance,
-        () => opacity,
-        11
-      ));
+      "Adjust Pitch",
+      () => "Left Stick",
+      () => activeMenu == 1,
+      alignment,
+      () => new Vector2(0f, 0f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
+      () => opacity,
+      11
+    ));
 
-    components.Add(new MenuItem(
-      "Adjust Throttle",
+    components.Add(new ControlItem(
+      "Increase Throttle",
+      () => "Right Trigger",
       () => activeMenu == 2,
       alignment,
       () => new Vector2(0f, menuSizeY) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
-    components.Add(new MenuItem(
-      "Adjust RCS",
+    components.Add(new ControlItem(
+      "Decrease Throttle",
+      () => "Left Trigger",
       () => activeMenu == 3,
       alignment,
       () => new Vector2(0f, menuSizeY * 2f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
-    components.Add(new MenuItem(
-      "Adjust Camera Zoom",
+    components.Add(new ControlItem(
+      "Adjust RCS",
+      () => "Left Stick",
       () => activeMenu == 4,
       alignment,
       () => new Vector2(0f, menuSizeY * 3f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
-    components.Add(new MenuItem(
-      "Move Camera",
+    components.Add(new ControlItem(
+      "Toggle RCS",
+      () => "X",
       () => activeMenu == 5,
       alignment,
       () => new Vector2(0f, menuSizeY * 4f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
-    components.Add(new MenuItem(
-      "Toggle SAS",
+    components.Add(new ControlItem(
+      "Toggle RCS Mode",
+      () => "Left Stick Click",
       () => activeMenu == 6,
       alignment,
       () => new Vector2(0f, menuSizeY * 5f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
-    components.Add(new MenuItem(
-      "Toggle RCS",
+    components.Add(new ControlItem(
+      "Toggle SAS",
+      () => "Y",
       () => activeMenu == 7,
       alignment,
       () => new Vector2(0f, menuSizeY * 6f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
-    components.Add(new MenuItem(
-      "Toggle RCS Mode",
+    components.Add(new ControlItem(
+      "Set SAS Mode",
+      () => "Directional Pad",
       () => activeMenu == 8,
       alignment,
       () => new Vector2(0f, menuSizeY * 7f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
-    components.Add(new MenuItem(
-      "Toggle Camera",
+    components.Add(new ControlItem(
+      "Move Camera",
+      () => "Right Stick",
       () => activeMenu == 9,
       alignment,
       () => new Vector2(0f, menuSizeY * 8f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
-    components.Add(new MenuItem(
-      "Toggle Camera Mode",
+    components.Add(new ControlItem(
+      "Adjust Camera Zoom",
+      () => "Right Stick",
       () => activeMenu == 10,
       alignment,
       () => new Vector2(0f, menuSizeY * 9f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
+      () => opacity,
+      11
+    ));
+
+    components.Add(new ControlItem(
+      "Toggle Camera",
+      () => "Back",
+      () => activeMenu == 11,
+      alignment,
+      () => new Vector2(0f, menuSizeY * 10f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
+      () => opacity,
+      11
+    ));
+
+    components.Add(new ControlItem(
+      "Toggle Camera Mode",
+      () => "Right Stick Click",
+      () => activeMenu == 12,
+      alignment,
+      () => new Vector2(0f, menuSizeY * 11f) + menuOffsetOverride + entireOffsetOverride,
+      controlItemDistance,
       () => opacity,
       11
     ));
 
     components.Add(new MenuItem(
       "Back",
-      () => activeMenu == 11,
+      () => activeMenu == 13,
       alignment,
-      () => new Vector2(0f, menuSizeY * 10.5f) + menuOffsetOverride + entireOffsetOverride,
+      () => new Vector2(0f, menuSizeY * 12.5f) + menuOffsetOverride + entireOffsetOverride,
       () => opacity,
       11
     ));
@@ -149,7 +189,7 @@ public class ControlsMenu : CustomGameComponent
 
   public override void Initialize()
   {
-    menuItemsLength = 11;
+    menuItemsLength = 13;
     activeMenu = 1;
 
     base.Initialize();
@@ -200,7 +240,7 @@ public class ControlsMenu : CustomGameComponent
       if (input.MenuUp())
         activeMenu--;
 
-      if ((activeMenu == 11 && input.MenuSelect()) || input.MenuBack())
+      if ((activeMenu == 13 && input.MenuSelect()) || input.MenuBack())
         state = State.Settings;
     }
 
